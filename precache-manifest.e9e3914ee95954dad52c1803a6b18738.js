cd09060ef68b533d1a1a64d0496859cd52078ce2
David <43558095+djacovini@users.noreply.github.com>
2019-04-05 00:13:04 -0400

    Updates

---
+++ precache-manifest.e9e3914ee95954dad52c1803a6b18738.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae73efe935c286a8adc8",
+    "url": "/Clicky_Game/static/js/runtime~main.1f82a145.js"
+  },
+  {
+    "revision": "5d9f1618bb0a5b50244c",
+    "url": "/Clicky_Game/static/js/main.0779edc9.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Clicky_Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d9f1618bb0a5b50244c",
+    "url": "/Clicky_Game/static/css/main.8eed6a1f.chunk.css"
+  },
+  {
+    "revision": "6de841a30a6f26ac986894c44a8999a4",
+    "url": "/Clicky_Game/index.html"
+  }
+];
